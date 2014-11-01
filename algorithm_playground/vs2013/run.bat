9aee3e6a04c7d38dfb1ba4d07b6db9172a2e2a26
任亚飞 <ren.yafee@gmail.com>
2014-11-01 19:02:37 +0800

    EVENODD Unit Test
    EVENODD模式下，带洞的多边形，内外path的顶点次数不起作用。
    ic 只内部逆时针，
    oc只外部逆时针
    ic_oc内外都逆时针

--- algorithm_playground/vs2013/run.bat
+++ algorithm_playground/vs2013/run.bat
@@ -1,2 +1,24 @@
-@Rem 存在洞的多边形，必须使用两个path表示，内部逆时针，外部顺时针
+@Rem http://zh.wikipedia.org/wiki/MS-DOS%E5%91%BD%E4%BB%A4%E5%88%97%E8%A1%A8
+
+@Rem 存在洞的多边形，必须使用两个path表示
+@Rem clip.txt中两个path都是顺时针方向存储
 clipper.exe subj.txt clip.txt UNION EVENODD EVENODD 1 30
+
+del solution1.svg
+ren solution.svg solution1.svg
+
+@Rem clip-oc.txt中外部path为逆时针，内部为顺时针
+clipper.exe subj.txt clip-oc.txt UNION EVENODD EVENODD 1 30
+del solution2.svg
+ren solution.svg solution2.svg
+
+@Rem clip-ic.txt中外部path为顺时针，内部为逆时针
+clipper.exe subj.txt clip-ic.txt UNION EVENODD EVENODD 1 30
+del solution3.svg
+ren solution.svg solution3.svg
+
+@Rem clip-ic_oc.txt中两个path都是逆时针方向存储
+clipper.exe subj.txt clip-ic_oc.txt UNION EVENODD EVENODD 1 30
+
+@Rem 设置字符串 set info=1,1 \n 2,2
+@Rem 打印字符串，并写到2文件中 echo %info% > 2
