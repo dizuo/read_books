9c71cfa963c7899370e77f06f5e037227f30c495
任亚飞 <ren.yafee@gmail.com>
2016-05-04 09:41:52 +0800

    optim
    ~

--- algorithm_playground/algorithm_playground/macro_show.cpp
+++ algorithm_playground/algorithm_playground/macro_show.cpp
@@ -14,6 +14,12 @@ DECLARE_MAIN_ENTRY(macro_unit_test);
     type get##firstBigName() const { return m##firstBigName;} \
 	className##* firstSmallName(type t) { m##firstBigName = t; return this; }
 
+// in XCode and MSVC good.
+#define SIMPLE_SETUP_POD_MEMBERS_(className, firstSmallName, firstBigName, type) \
+    type m##firstBigName;    \
+    type get##firstBigName() const { return m##firstBigName;} \
+	className* firstSmallName(type t) { m##firstBigName = t; return this; }
+
 class Test
 {
 public:
@@ -22,18 +28,53 @@ public:
 	Test* fontSize(int t) { mFontSize = t; return this; }
 	int getFontSize() const { return mFontSize; }
 
+	SIMPLE_SETUP_POD_MEMBERS_(Test, bgColor, BgColor, int);
+
 	SETUP_POD_MEMBERS_(Test, frontColor, FrontColor, int);
+
+};
+
+template< typename T>
+class WrapperClass
+{
+public:
+	T* getObj() { return new T(); }
+
+};
+
+class XTest : public WrapperClass<XTest>
+{
+	friend class WrapperClass<XTest>;
+
+	XTest() {}
+	~XTest() {}
+};
+
+class XMarker : public WrapperClass<XMarker>
+{
+	friend class WrapperClass<XMarker>;
+
+	XMarker() {}
+	~XMarker() {}
+};
+
+class Manager : public WrapperClass<XMarker>
+{
+public:
 
 };
 
 void macro_unit_test()
 {
 	Test test;
-	Test* ptest = test.fontSize(12)->frontColor(10);
+	Test* ptest = test.fontSize(12)->frontColor(10)->bgColor(9);
 
 	int fs = ptest->getFontSize();
 	int fc = ptest->getFrontColor();
 	printf("%d, %d\n", fs, fc);
+
+	Manager mgr;
+	XMarker* pm = mgr.getObj();
 
 }
 
