e2cf1d9cf83cf4bbc2cfcfde50442243f11f0676
任亚飞 <ren.yafee@gmail.com>
2017-05-07 11:09:03 +0800

    upgrade
    1. file io use C++ style.
    2. render API, use VertexArray.

--- algorithm_playground/gl_render/tess_helper.cpp
+++ algorithm_playground/gl_render/tess_helper.cpp
@@ -1,5 +1,3 @@
-#define _CRT_SECURE_NO_WARNINGS
-
 #include "tess_helper.h"
 
 #include "gl/glut.h"
@@ -10,6 +8,12 @@
 #include <string>
 
 using namespace std;
+
+namespace rd
+{
+	void render_lines_impl(vertices_t& path);
+	void render_triangles_impl(std::list<TPPLPoly>&);
+}
 
 void make_poly(float* buf, int pnt_sz, TPPLPoly& poly)
 {
@@ -137,35 +141,8 @@ void TessHelper::render()
 	if (path.size() < 2)
 		return;
 
-	glColor4ub(255, 255, 0, 256);
-
-	for (int k(0); k < path.size(); k++)
-	{
-		int next = (k + 1) % path.size();
-		glBegin(GL_LINES);
-		glVertex2f(path[k].x(), path[k].y());
-		glVertex2f(path[next].x(), path[next].y());
-		glEnd();
-	}
-
-#if 0
-	std::vector<float> vert_vec;
-	for (int k(0); k < path.size(); k++)
-	{
-		vert_vec.push_back(path[k].x());
-		vert_vec.push_back(path[k].y());
-	}
-
-	std::vector<int> indices_vec;
-	for (int k(0); k < path.size(); k++)
-	{
-		int next = (k + 1) % path.size();
-		indices_vec.push_back(k);
-		indices_vec.push_back(next);
-	}
-	glVertexPointer(2, GL_FLOAT, 0, &vert_vec[0]);
-	glDrawElements(GL_LINES, indices_vec.size(), GL_UNSIGNED_INT, &indices_vec[0]);
-#endif
+	rd::render_lines_impl(this->path);
+	rd::render_lines_impl(this->hole);
 
 	if (this->path.size() < 3)
 		return;
@@ -193,23 +170,7 @@ void TessHelper::render()
 	}
 
 	// Draw polygons.
-	glColor4ub(255, 255, 255, 256);
-
-	glPushMatrix();
-	// glTranslatef(200, 200, 0);
-	for (poly_iter_t it = triangles.begin(); it != triangles.end(); it++)
-	{
-		TPPLPoly& poly = *it;
-
-		glBegin(GL_TRIANGLES);
-		for (int k(0); k < poly.GetNumPoints(); k++)
-		{
-			glVertex2f(poly.GetPoint(k).x, poly.GetPoint(k).y);
-		}
-		glEnd();
-
-	}
-	glPopMatrix();
+	rd::render_triangles_impl(triangles);
 
 }
 
@@ -252,30 +213,30 @@ void TessHelper::clear()
 	hole.clear();
 }
 
-void dump(FILE* fp, vertices_t& path, const char* tag)
+void dump(std::ofstream& ostream, vertices_t& path, const char* tag)
 {
-	fprintf(fp, "%s %d\n", tag, path.size());
+	ostream << tag << " " << path.size() << std::endl;
 
 	for (int k(0); k < path.size(); k++)
 	{
-		fprintf(fp, "%f %f\n", path[k].x(), path[k].y());
+		ostream << path[k].x() << " " << path[k].y() << std::endl;
 	}
-	fflush(fp);
+	ostream.flush();
 }
 
 void TessHelper::save(const char* fname)
 {
-	FILE* fp = fopen(fname, "w");
-	if (!fp) return;
+	std::ofstream outfile;
+	outfile.open(fname);
+	if (!outfile.good())
+		return;
 
-	dump(fp, this->path, "path");
+	dump(outfile, this->path, "path");
 
 	if (!hole.empty())
 	{
-		dump(fp, this->hole, "hole");
+		dump(outfile, this->hole, "hole");
 	}
-
-	fclose(fp);
 }
 
 void TessHelper::restore(const char* fname)
@@ -315,3 +276,86 @@ void TessHelper::restore(const char* fname)
 	
 }
 
+// ---------------------------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------------------
+#define INSTANCE_RENDER 0
+
+namespace rd
+{ 
+void render_lines_impl(vertices_t& path)
+{
+	if (path.size() < 2)
+		return;
+
+	glLineWidth(2.0f);
+	glColor4ub(255, 255, 0, 256);
+
+#if INSTANCE_RENDER
+	for (int k(0); k < path.size(); k++)
+	{
+		int next = (k + 1) % path.size();
+		glBegin(GL_LINES);
+		glVertex2f(path[k].x(), path[k].y());
+		glVertex2f(path[next].x(), path[next].y());
+		glEnd();
+	}
+#else
+	std::vector<float> vert_vec;
+	for (int k(0); k < path.size(); k++)
+	{
+		vert_vec.push_back(path[k].x());
+		vert_vec.push_back(path[k].y());
+	}
+
+	std::vector<int> indices_vec;
+	for (int k(0); k < path.size(); k++)
+	{
+		int next = (k + 1) % path.size();
+		indices_vec.push_back(k);
+		indices_vec.push_back(next);
+	}
+
+	glEnableClientState(GL_VERTEX_ARRAY);
+	glVertexPointer(2, GL_FLOAT, 0, &vert_vec[0]);
+	glDrawElements(GL_LINES, indices_vec.size(), GL_UNSIGNED_INT, &indices_vec[0]);
+#endif
+}
+
+void render_triangles_impl(std::list<TPPLPoly>& triangles)
+{
+	glColor4ub(255, 255, 255, 256);
+
+#if INSTANCE_RENDER
+	for (poly_iter_t it = triangles.begin(); it != triangles.end(); it++)
+	{
+		TPPLPoly& poly = *it;
+
+		glBegin(GL_TRIANGLES);
+		for (int k(0); k < poly.GetNumPoints(); k++)
+		{
+			glVertex2f(poly.GetPoint(k).x, poly.GetPoint(k).y);
+		}
+		glEnd();
+	}
+#else
+	std::vector<float> vert_vec;
+	int vert_num = triangles.size() * 3;
+	vert_vec.reserve(vert_num * 2);
+
+	for (poly_iter_t it = triangles.begin(); it != triangles.end(); it++)
+	{
+		TPPLPoly& poly = *it;
+
+		for (int k(0); k < 3; k++)
+		{
+			vert_vec.push_back(poly.GetPoint(k).x);
+			vert_vec.push_back(poly.GetPoint(k).y);
+		}
+	}
+
+	glEnableClientState(GL_VERTEX_ARRAY);
+	glVertexPointer(2, GL_FLOAT, 0, &vert_vec[0]);
+	glDrawArrays(GL_TRIANGLES, 0, vert_num);
+#endif
+}
+}
